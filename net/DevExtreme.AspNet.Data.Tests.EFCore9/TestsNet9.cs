37978f1ff6e38da88efa7512fd5a20f46f8b94df
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2025-01-20 13:37:45 +0200

    ci: windows-latest

---
+++ net/DevExtreme.AspNet.Data.Tests.EFCore9/TestsNet9.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+
+namespace DevExtreme.AspNet.Data.Tests.EFCore9 {
+
+    public class TestsNet9 {
+        [Fact]
+        public void AssertImageRuntime9() {
+            Assert.Fail("AssertImageRuntime9 -> Fail");
+        }
+    }
+}
