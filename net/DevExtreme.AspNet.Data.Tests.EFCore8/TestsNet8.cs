f9f3292b6f0205c6db2ffcb2d0eeb73ebcafb191
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2025-02-14 12:41:28 +0200

    ci: ubuntu-latest

---
+++ net/DevExtreme.AspNet.Data.Tests.EFCore8/TestsNet8.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+
+namespace DevExtreme.AspNet.Data.Tests.EFCore8 {
+
+    public class TestsNet8 {
+        [Fact]
+        public void AssertImageRuntime8() {
+            Assert.Fail("AssertImageRuntime8 -> Fail");
+        }
+    }
+}
